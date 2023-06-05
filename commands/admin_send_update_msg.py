b8fe48ad1fb1304d0a739e2ae7ea16c2a01d72ca
Nikita Orlov <orlov.nickita@gmail.com>
2023-06-05 13:59:30 +0300

    обновил команду отправки уведомлений обновлений

--- commands/admin_send_update_msg.py
+++ commands/admin_send_update_msg.py
@@ -3,17 +3,25 @@
 """
 
 import time
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.types import Message, ParseMode, CallbackQuery
-
 from keyboards_for_bot.admin_keyboards import IKM_admin_update_send_conf
-from loader import bot, hdd_dir, update_jgp, admin_manual_name, admin_manual_dir, administrators, info_jgp, \
-    users_sql_dir, users_sql_file_name, dp
+from loader import bot, administrators, dp
 from loader import users_sql_dir, users_sql_file_name
 from utils.custom_funcs import button_text
 from utils.logger import logger
 from utils.sql_funcs import get_info_from_sql_for_followers
 
-update_msg_text = ''
+
+class UpdateMsgSend(StatesGroup):
+    """
+    Машина состояний
+    TODO
+    """
+    get_msg = State()
+    send_msg = State()
+    update_msg_text = ''
 
 
 async def start(message: Message) -> None:
@@ -26,53 +34,36 @@ async def start(message: Message) -> None:
                 username=message.from_user.username,
                 user_id=message.chat.id)
     
-    # all_persons = get_info_from_sql(sql_base=users_sql_dir + users_sql_file_name,
-    #                                 message=message)
-    
-    # all_persons = [459901923, 434895679, 949421028]
-    # all_persons = [949421028]
-    
-    # global update_msg_text
-    # update_msg_text = 'Уважаемые пользователи,\n' \
-    #                   '\n' \
-    #                   'Мы видим, что Вы запрашиваете расписание, но, к сожалению, мы еще не получили расписание от ' \
-    #                   'настоятеля Храма. Приносим свои извинения, уведомим Вас, когда расписание станет доступным'
-    #
-    # bot.send_photo(chat_id=message.chat.id,
-    #                photo=open(hdd_dir + update_jgp, 'rb'),
-    #                caption=f'Будет отправлено {len(all_persons)} сообщений.\n'
-    #                        f'Выглядеть будет так:\n'
-    #                        f'\n'
-    #                        f'{update_msg_text}\n'
-    #                        f'\n'
-    #                        f'Подтвердите отправку',
-    #                reply_markup=IKM_admin_update_send_conf())
     if message.chat.id == int(administrators['Никита']):
-        msg = bot.send_message(chat_id=message.chat.id,
-                               text='Что отправим?')
+        await UpdateMsgSend.get_msg.set()
         
-        bot.register_next_step_handler(message=msg, callback=preparation_before_send)
+        await bot.send_message(chat_id=message.chat.id,
+                               text='Что отправим?')
 
 
-async def preparation_before_send(message: Message) -> None:
+@dp.message_handler(content_types=['text'], state=UpdateMsgSend.get_msg)
+async def preparation_before_send(message: Message, state: FSMContext) -> None:
+    """
+    TODO
+    """
     logger.info(
         'Запущена функция preparation_before_send',
         username=message.from_user.username,
         user_id=message.chat.id)
     
-    global update_msg_text
-    update_msg_text = message.text
+    async with state.proxy() as data:
+        upd_txt = data['get_msg'] = message.text
     
-    await bot.send_photo(chat_id=message.chat.id,
-                         # photo=open(hdd_dir + update_jgp, 'rb'),
-                         photo=open(hdd_dir + info_jgp, 'rb'),
-                         caption=update_msg_text,
-                         reply_markup=IKM_admin_update_send_conf())
+    await bot.send_message(chat_id=message.chat.id,
+                           text=upd_txt,
+                           reply_markup=IKM_admin_update_send_conf())
+    
+    await UpdateMsgSend.send_msg.set()
 
 
 @dp.callback_query_handler(
-    lambda call: call.data == 'yes_send_upd_msg' or call.data == 'no_send_upd_msg')
-async def send_update_msg_confirm(call: CallbackQuery) -> None:
+    lambda call: call.data == 'yes_send_upd_msg' or call.data == 'no_send_upd_msg', state=UpdateMsgSend.send_msg)
+async def send_update_msg_confirm(call: CallbackQuery, state: FSMContext) -> None:
     """
     Обработка нажатия на клавиатуре с подтверждением отправки сообщения с обновлениями
     param call: Нажатая кнопка на клавиатуре
@@ -84,6 +75,8 @@ async def send_update_msg_confirm(call: CallbackQuery) -> None:
         user_id=call.message.chat.id)
     
     if call.data == 'no_send_upd_msg':
+        await state.finish()
+        
         await bot.delete_message(chat_id=call.message.chat.id,
                                  message_id=call.message.message_id)
         
@@ -94,19 +87,17 @@ async def send_update_msg_confirm(call: CallbackQuery) -> None:
                     user_id=call.message.chat.id)
     
     else:
-        global update_msg_text
+        async with state.proxy() as data:
+            upd_txt = data['get_msg'] = call.message.text
         
-        # all_persons = get_info_from_sql_for_followers(sql_base=users_sql_dir + users_sql_file_name,
-        #                                               message=call.message)
-        
-        # print(get_info_from_sql_for_followers(sql_base=users_sql_dir + users_sql_file_name,
-        #                                               message=call.message))
+        # all_persons = await get_info_from_sql_for_followers(sql_base=users_sql_dir + users_sql_file_name,
+        #                                                     message=call.message)
         
         # all_persons = [459901923, 434895679, 949421028]
         # all_persons = [949421028, 5022408096, 12444114124412421412]
-        all_persons = [(949421028,), (5022408096,)]
         # all_persons = [(5022408096,), (1201283009,), (1765957161,), (345707936,), (1692880786,)]
-        
+        all_persons = [(949421028,), (5022408096,)]
+
         count_txt = 0
         not_found = []
         not_send = []
@@ -114,16 +105,13 @@ async def send_update_msg_confirm(call: CallbackQuery) -> None:
             time.sleep(0.5)
             
             try:
-                await bot.send_photo(chat_id=person[0],
-                                     # photo=open(hdd_dir + update_jgp, 'rb'),
-                                     photo=open(hdd_dir + info_jgp, 'rb'),
-                                     caption=update_msg_text,
-                                     parse_mode=ParseMode.HTML)
+                await bot.send_message(chat_id=person[0],
+                                       text=upd_txt,
+                                       parse_mode=ParseMode.HTML)
                 count_txt += 1
                 
                 logger.info('Сообщение № {}.\n'
-                            'Отправлено пользователю {}'.format(count_txt,
-                                                                person[0]),
+                            'Отправлено пользователю {}'.format(count_txt, person[0]),
                             user_id=call.message.chat.id)
             
             # except telebot.apihelper.ApiTelegramException:
@@ -156,10 +144,12 @@ async def send_update_msg_confirm(call: CallbackQuery) -> None:
                  'Не найдены пользователи: {not_found}\n'
                  '\n'
                  'Не отправлено пользователям из-за ошибок: {not_send}'.format(
-                new_msg=update_msg_text,
-                qty_m=count_txt - len(not_found) - len(not_send),
+                new_msg=upd_txt,
+                qty_m=count_txt,
                 not_found=not_found,
                 not_send=not_send))
         
         logger.info(t.text,
                     user_id=call.message.chat.id)
+        
+        await state.finish()
