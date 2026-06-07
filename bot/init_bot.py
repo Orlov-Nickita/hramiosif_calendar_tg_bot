8b2b4cf35fb37772c9687726a965156d22170c13
Nikita Orlov <orlov.nickita@gmail.com>
2026-06-07 14:29:20 +0300

    proxy

--- bot/init_bot.py
+++ bot/init_bot.py
@@ -2,15 +2,14 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-
+PROXY = os.getenv('PROXY')
 
 def init_bot():
-    bot = Bot(token=BOT_TOKEN)
+    bot = Bot(token=BOT_TOKEN, proxy=PROXY)
     dp = Dispatcher(bot, storage=MemoryStorage())
     dp.middleware.setup(LoggingMiddleware())
 
